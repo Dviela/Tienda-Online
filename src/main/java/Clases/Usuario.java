c896df4b0304d062993891f4fa10ac1e30da004e
Dviela <a19230@svalero.com>
2022-05-28 15:52:41 +0200

    Añadida funcionalidad para Registro de usuarios

--- src/main/java/Clases/Usuario.java
+++ src/main/java/Clases/Usuario.java
@@ -1,7 +1,11 @@
 package Clases;
 
+import java.util.List;
+
 public class Usuario {
     private long id;
     private String nombreUsuario;
     private String contrasena;
+
+    private List<Usuario> usuarios;
 }
